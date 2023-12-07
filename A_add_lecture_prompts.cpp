9a02dc475563dae4ae63d314f35f58a9f16032a2
ChilledPi <chilledpi7@gmail.com>
2023-12-08 01:29:57 +0900

    Fin.

--- A_add_lecture_prompts.cpp
+++ A_add_lecture_prompts.cpp
@@ -19,76 +19,61 @@ extern vector<string> classrooms;
 // 학사팀(A) 강의 추가 프롬프트
 // 강의 정보들을 입력받고, 규칙 및 중복 검사
 // 이후 강의번호의 존재 여부에 따라 수정 혹은 추가
+
 void A_add_lecture_prompt() {
   Lecture new_lecture;
-  int prof_id_index;
   vector<tp> timeplace;
-  std::string weekday;
-  std::string lecture_time;
-  int room_id_index;
-  int check;
+  string input_credit;
 
   // 강의번호 입력
   while (true) {
     std::cout << "추가할 강의번호를 입력해주세요 > ";
-    if((new_lecture.num = read_num()) != ""){
+    if ((new_lecture.num = read_num()) != "") {
       break;
     }
   }
-  
+
   // 강의명 입력
   while (true) {
     std::cout << "추가할 강의명을 입력해주세요 > ";
-    if((new_lecture.name = read_name()) != ""){
+    if ((new_lecture.name = read_name()) != "") {
       break;
     }
   }
 
-  while(true){
-    for (int i = 0; i < professor.size(); i++){
-      cout << i + 1 << ") " << professor[i].second << '\n';
-    }
-    std::cout << "추가할 교강사의 번호(교번 X)를 입력해주세요 > ";
-    prof_id_index = check_num_input(professor.size());
-    if(prof_id_index > 0){
-      // new_lecture.professor_id = professor[prof_id_index - 1].first; 프로페서did
-      break;
-    }
-    if (prof_id_index == 0) {
-      std::cout
-          << "! 0번 항목이 존재하지 않습니다." << std::endl
-          << "------------------------------------------------------------"
-             "--------------"
-          << std::endl;
-    }
-  }
-
-// 요일, 강의시간(교시), 강의실 입력
-//   check = 0;
+  // 교번 선택, 요일/시간 입력, 강의실 선택
   while (true) {
-    tp new_tp;
-    // 요일 입력
-    while (true) {
-      string temp;
-      std::cout << "추가할 강의의 요일과 강의시간을 입력해주세요 > ";
-      if((temp = read_time()) != ""){
-        new_tp.day = Day((temp[0]) - '0'); // !!!!!!!!!!!
-        new_tp.time = temp.substr(1, 4);
-        break;
-      }
-    }
+    // 교번 선택
 
+    int prof_choice;  // 사용자 선택 교번 번호 저장
+    vector<int>
+        prof_choice_list;  // 이전 반복까지의 사용자 선택 교번 번호들 저장
+    int number_of_prof = 0;  // (현재까지) 저장할 교강사 수
     while (true) {
-      for (int i = 0; i < classrooms.size(); i++) {
-        cout << i + 1 << ") " << classrooms[i] << '\n';
+      for (int i = 0, j = 0; i < professor.size() - number_of_prof; i++, j++) {
+        if (find(prof_choice_list.begin(), prof_choice_list.end(), i + 1) ==
+            prof_choice_list.end()) {
+          std::cout << j + 1 << ") " << professor[i].second << std::endl;
+        } else
+          j--;
       }
-      std::cout << "추가할 강의의 강의실을 입력해주세요 > ";
-      room_id_index = check_num_input(classrooms.size());
-      if (room_id_index > 0) {
-        new_tp.classroom = classrooms[room_id_index - 1];
-        break;
+      std::cout << "추가할 교강사의 번호(교번 X)를 입력해주세요 > ";
+      prof_choice = check_num_input(professor.size() - number_of_prof);
+      if (prof_choice > 0) {
+        new_lecture.professor_id_list.push_back(
+            professor[prof_choice - 1].first);
+        prof_choice_list.push_back(prof_choice);
+        std::cout << "1) 예" << std::endl
+                  << "0) 아니오" << std::endl
+                  << "교강사를 더 추가하시겠습니까? > ";
+        int choice = check_num_input(2);
+        if (choice == 1) {
+          number_of_prof++;
+          continue;
+        } else if (choice == 0)
+          break;
       }
-      if (room_id_index == 0) {
+      if (prof_choice == 0) {  // check_num_input은 0을 오류처리하지 않음
         std::cout
             << "! 0번 항목이 존재하지 않습니다." << std::endl
             << "------------------------------------------------------------"
@@ -97,58 +82,175 @@ void A_add_lecture_prompt() {
       }
     }
 
-    if(is_expendable(timeplace, new_tp.day, new_tp.time)){
-      timeplace.push_back(new_tp);
-    }else{
-      std::cout << "! 요일/시간이 겹칩니다."
-                << std::endl;
-      std::cout
-          << "------------------------------------------------------------"
-             "--------------\n";
-      continue;
-    }
-
-    if (!room_is_addable(new_tp, classrooms[room_id_index - 1]) &&
-        find(all_classes_list.begin(), all_classes_list.end(),
-             Lecture(new_lecture.num)) ==
-            all_classes_list.end()) {  // 강의실 꽉차면 나갈 방법이 없음
-      std::cout << "! 해당 요일/시간에 강의실이 이미 사용중입니다."
-                << std::endl;
-      std::cout
-          << "------------------------------------------------------------"
-             "--------------\n";
-      continue;
-    }
-
-    // if (!prof_is_addable(professor[prof_id_index - 1].first, timeplace) &&
-    //     find(all_classes_list.begin(), all_classes_list.end(),
-    //          Lecture(new_lecture.num)) ==
-    //         all_classes_list.end()) {  // 교강사 역시 마찬가지
-    //   std::cout << "! 해당 요일/시간에 해당 교강사가 이미 강의중입니다."
-    //             << std::endl;
-    //   std::cout
-    //       << "------------------------------------------------------------"
-    //          "--------------\n";
-    //   continue;
-    // }
-
-    int sel;
+    // 요일, 강의시간(교시), 강의실 입력
     while (true) {
-      std::cout << "1) 예" << std::endl
-                << "0) 아니오" << std::endl
-                << "강의 일시를 더 추가하시겠습니까? > ";
-      if ((sel = check_num_input(1)) >= 0) {
-        new_lecture.tp_list.push_back(new_tp);
-        break;
+      tp new_tp;
+
+      // 요일 입력
+      while (true) {
+        string temp;
+        std::cout << "추가할 강의의 요일과 강의시간을 입력해주세요 > ";
+        if ((temp = read_time()) != "") {
+          new_tp.day = Day((temp[0]) - '0');  // !!!!!!!!!!!
+          new_tp.time = temp.substr(1, 4);
+          break;
+        }
+      }
+
+      // 강의실 입력
+      int room_id_index;
+      while (true) {
+        for (int i = 0; i < classrooms.size(); i++) {
+          cout << i + 1 << ") " << classrooms[i] << '\n';
+        }
+        std::cout << "추가할 강의의 강의실을 입력해주세요 > ";
+        room_id_index = check_num_input(classrooms.size());
+        if (room_id_index > 0) {
+          new_tp.classroom = classrooms[room_id_index - 1];
+          break;
+        }
+        if (room_id_index == 0) {
+          std::cout
+              << "! 0번 항목이 존재하지 않습니다." << std::endl
+              << "------------------------------------------------------------"
+                 "--------------"
+              << std::endl;
+        }
+      }
+
+      if (is_expendable(timeplace, new_tp.day, new_tp.time)) {
+        timeplace.push_back(new_tp);
+      } else {
+        std::cout << "! 요일/시간이 겹칩니다." << std::endl;
+        std::cout
+            << "------------------------------------------------------------"
+               "--------------\n";
+        continue;
+      }
+
+      if (!room_is_addable(new_tp, classrooms[room_id_index - 1]) &&
+          find(all_classes_list.begin(), all_classes_list.end(),
+               Lecture(new_lecture.num)) ==
+              all_classes_list.end()) {  // 강의실 꽉차면 나갈 방법이 없음
+        std::cout << "! 해당 요일/시간에 강의실이 이미 사용중입니다."
+                  << std::endl;
+        std::cout
+            << "------------------------------------------------------------"
+               "--------------\n";
+        continue;
+      }
+
+      // if (!prof_is_addable(professor[prof_id_index - 1].first, timeplace) &&
+      //     find(all_classes_list.begin(), all_classes_list.end(),
+      //          Lecture(new_lecture.num)) ==
+      //         all_classes_list.end()) {  // 교강사 역시 마찬가지
+      //   std::cout << "! 해당 요일/시간에 해당 교강사가 이미 강의중입니다."
+      //             << std::endl;
+      //   std::cout
+      //       << "------------------------------------------------------------"
+      //          "--------------\n";
+      //   continue;
+      // }
+
+      int sel;
+      while (true) {
+        std::cout << "1) 예" << std::endl
+                  << "0) 아니오" << std::endl
+                  << "강의 일시를 더 추가하시겠습니까? > ";
+        if ((sel = check_num_input(1)) >= 0) {
+          new_lecture.tp_list.push_back(new_tp);
+          break;
+        }
+      }
+      if (sel == 0) break;
+    }
+
+    // 교강사 시간 확인 및 팀티칭 확인
+
+    all_classes_list.push_back(new_lecture);
+    if (prof_integrity()) {
+      std::cout << "해당 요일/시간에 해당 교강사가 이미 강의중입니다."
+                << std::endl;
+      all_classes_list.pop_back();
+      // new_lecture 초기화 ?
+      continue;
+    } else
+      break;
+  }
+
+  // 학점 입력
+  while (true) {
+
+    std::cout << "추가할 학점을 입력해주세요 > ";
+    getline(cin, input_credit);
+    input_credit = trim(input_credit);
+    if (stoi(input_credit) >= 1 && stoi(input_credit) <= 3) {
+      new_lecture.credit = input_credit;
+      std::cout
+          << "------------------------------------------------------------"
+             "--------------\n";
+      break;
+    } else
+      std::cout
+          << "! 학점은 1, 2, 3 중 하나여야 합니다." << std::endl
+          << "------------------------------------------------------------"
+             "--------------\n";
+    continue;
+  }
+
+  int time1 = 0;
+  for (int i = 0; i < new_lecture.tp_list.size(); i++) {
+    int a = stoi(new_lecture.tp_list.at(i).time);
+    int b = a % 100;
+    a /= 100;
+    if (a == 19) {
+      a = -1;
+    }
+    if (b == 19) {
+      b = -1;
+    }
+    int c = b - a;
+    time1 += c;
+  }
+
+  for (int i = 0; i < all_classes_list.size(); i++) {
+    if (new_lecture.name == all_classes_list.at(i).name) {
+      int time2 = 0;
+      if (input_credit != all_classes_list.at(i).credit) {
+        std::cout
+            << "! 다른 분반과 학점이 다릅니다." << std::endl
+            << "------------------------------------------------------------"
+               "--------------\n";
+        return;
+      }
+      for (int j = 0; j < all_classes_list.at(i).tp_list.size(); j++) {
+        int a = stoi(all_classes_list.at(i).tp_list.at(j).time);
+        int b = a % 100;
+        a /= 100;
+        if (a == 19) {
+          a = -1;
+        }
+        if (b == 19) {
+          b = -1;
+        }
+        int c = b - a;
+        time2 += c;
+      }
+      if (time1 != time2) {
+        std::cout
+            << "! 다른 분반과 수업 시수가 다릅니다." << std::endl
+            << "------------------------------------------------------------"
+               "--------------\n";
+        return;
       }
     }
-    if (sel == 0) break;
   }
 
   // 강의번호 이미 존재하는지 검사
   std::cout << "동일한 강의번호의 존재유무를 확인합니다." << std::endl;
 
   // 중복 강의번호 있음
+  int check;
   if (find(all_classes_list.begin(), all_classes_list.end(),
            Lecture(new_lecture.num)) != all_classes_list.end()) {
     std::cout << "중복되는 강의 번호가 존재합니다!" << std::endl
@@ -162,22 +264,20 @@ void A_add_lecture_prompt() {
                 << "정말 갱신하시겠습니까? > ";
       check = check_num_input(1);
       if (check == 1) {
-            modify_lecture(new_lecture); // 자료구조 수정, 학생이 넣은 강의는 삭제해야함
-            std::cout << "강의 정보를 갱신합니다." << std::endl
-                      << "-----------------------------------------------------"
-                         "-------"
-                         "--------------"
-                      << std::endl;
-            return;
+        modify_lecture(new_lecture);  // 자료구조 수정, 학생이 넣은 강의는 삭제
+        std::cout << "강의 정보를 갱신합니다." << std::endl
+                  << "-----------------------------------------------------"
+                     "---------------------"
+                  << std::endl;
+        return;
       } else if (check == 0) {
-            std::cout << "강의 정보를 갱신하지 않았습니다." << std::endl
-                      << "-----------------------------------------------------"
-                         "-------"
-                         "--------------"
-                      << std::endl;
-            return;
+        std::cout << "강의 정보를 갱신하지 않았습니다." << std::endl
+                  << "-----------------------------------------------------"
+                     "---------------------"
+                  << std::endl;
+        return;
       } else {
-            continue;
+        continue;
       }
     }
   }
@@ -206,6 +306,194 @@ void A_add_lecture_prompt() {
     return;
   }
 }
+
+// void A_add_lecture_prompt() {
+//   Lecture new_lecture;
+//   int prof_id_index;
+//   vector<tp> timeplace;
+//   std::string weekday;
+//   std::string lecture_time;
+//   int room_id_index;
+//   int check;
+
+//   // 강의번호 입력
+//   while (true) {
+//     std::cout << "추가할 강의번호를 입력해주세요 > ";
+//     if((new_lecture.num = read_num()) != ""){
+//       break;
+//     }
+//   }
+  
+//   // 강의명 입력
+//   while (true) {
+//     std::cout << "추가할 강의명을 입력해주세요 > ";
+//     if((new_lecture.name = read_name()) != ""){
+//       break;
+//     }
+//   }
+
+//   while(true){
+//     for (int i = 0; i < professor.size(); i++){
+//       cout << i + 1 << ") " << professor[i].second << '\n';
+//     }
+//     std::cout << "추가할 교강사의 번호(교번 X)를 입력해주세요 > ";
+//     prof_id_index = check_num_input(professor.size());
+//     if(prof_id_index > 0){
+//       // new_lecture.professor_id = professor[prof_id_index - 1].first; 프로페서did
+//       break;
+//     }
+//     if (prof_id_index == 0) {
+//       std::cout
+//           << "! 0번 항목이 존재하지 않습니다." << std::endl
+//           << "------------------------------------------------------------"
+//              "--------------"
+//           << std::endl;
+//     }
+//   }
+
+// // 요일, 강의시간(교시), 강의실 입력
+// //   check = 0;
+//   while (true) {
+//     tp new_tp;
+//     // 요일 입력
+//     while (true) {
+//       string temp;
+//       std::cout << "추가할 강의의 요일과 강의시간을 입력해주세요 > ";
+//       if((temp = read_time()) != ""){
+//         new_tp.day = Day((temp[0]) - '0'); // !!!!!!!!!!!
+//         new_tp.time = temp.substr(1, 4);
+//         break;
+//       }
+//     }
+
+//     while (true) {
+//       for (int i = 0; i < classrooms.size(); i++) {
+//         cout << i + 1 << ") " << classrooms[i] << '\n';
+//       }
+//       std::cout << "추가할 강의의 강의실을 입력해주세요 > ";
+//       room_id_index = check_num_input(classrooms.size());
+//       if (room_id_index > 0) {
+//         new_tp.classroom = classrooms[room_id_index - 1];
+//         break;
+//       }
+//       if (room_id_index == 0) {
+//         std::cout
+//             << "! 0번 항목이 존재하지 않습니다." << std::endl
+//             << "------------------------------------------------------------"
+//                "--------------"
+//             << std::endl;
+//       }
+//     }
+
+//     if(is_expendable(timeplace, new_tp.day, new_tp.time)){
+//       timeplace.push_back(new_tp);
+//     }else{
+//       std::cout << "! 요일/시간이 겹칩니다."
+//                 << std::endl;
+//       std::cout
+//           << "------------------------------------------------------------"
+//              "--------------\n";
+//       continue;
+//     }
+
+//     if (!room_is_addable(new_tp, classrooms[room_id_index - 1]) &&
+//         find(all_classes_list.begin(), all_classes_list.end(),
+//              Lecture(new_lecture.num)) ==
+//             all_classes_list.end()) {  // 강의실 꽉차면 나갈 방법이 없음
+//       std::cout << "! 해당 요일/시간에 강의실이 이미 사용중입니다."
+//                 << std::endl;
+//       std::cout
+//           << "------------------------------------------------------------"
+//              "--------------\n";
+//       continue;
+//     }
+
+//     // if (!prof_is_addable(professor[prof_id_index - 1].first, timeplace) &&
+//     //     find(all_classes_list.begin(), all_classes_list.end(),
+//     //          Lecture(new_lecture.num)) ==
+//     //         all_classes_list.end()) {  // 교강사 역시 마찬가지
+//     //   std::cout << "! 해당 요일/시간에 해당 교강사가 이미 강의중입니다."
+//     //             << std::endl;
+//     //   std::cout
+//     //       << "------------------------------------------------------------"
+//     //          "--------------\n";
+//     //   continue;
+//     // }
+
+//     int sel;
+//     while (true) {
+//       std::cout << "1) 예" << std::endl
+//                 << "0) 아니오" << std::endl
+//                 << "강의 일시를 더 추가하시겠습니까? > ";
+//       if ((sel = check_num_input(1)) >= 0) {
+//         new_lecture.tp_list.push_back(new_tp);
+//         break;
+//       }
+//     }
+//     if (sel == 0) break;
+//   }
+
+//   // 강의번호 이미 존재하는지 검사
+//   std::cout << "동일한 강의번호의 존재유무를 확인합니다." << std::endl;
+
+//   // 중복 강의번호 있음
+//   if (find(all_classes_list.begin(), all_classes_list.end(),
+//            Lecture(new_lecture.num)) != all_classes_list.end()) {
+//     std::cout << "중복되는 강의 번호가 존재합니다!" << std::endl
+//               << "------------------------------------------------------------"
+//                  "--------------"
+//               << std::endl;
+
+//     while (true) {
+//       std::cout << "1) 예" << std::endl
+//                 << "0) 아니오" << std::endl
+//                 << "정말 갱신하시겠습니까? > ";
+//       check = check_num_input(1);
+//       if (check == 1) {
+//             modify_lecture(new_lecture); // 자료구조 수정, 학생이 넣은 강의는 삭제해야함
+//             std::cout << "강의 정보를 갱신합니다." << std::endl
+//                       << "-----------------------------------------------------"
+//                          "-------"
+//                          "--------------"
+//                       << std::endl;
+//             return;
+//       } else if (check == 0) {
+//             std::cout << "강의 정보를 갱신하지 않았습니다." << std::endl
+//                       << "-----------------------------------------------------"
+//                          "-------"
+//                          "--------------"
+//                       << std::endl;
+//             return;
+//       } else {
+//             continue;
+//       }
+//     }
+//   }
+
+//   // 중복 강의번호 없음
+//   std::cout << "중복되는 강의 번호가 없습니다!" << std::endl
+//             << "------------------------------------------------------------"
+//                "--------------"
+//             << std::endl
+//             << "1) 예" << std::endl
+//             << "0) 아니오" << std::endl
+//             << "해당 강의를 추가하시겠습니까? > ";
+//   check = check_num_input(1);
+//   if (check == 1) {
+//     all_classes_list.push_back(new_lecture);
+//     std::cout << "강의를 추가하였습니다." << std::endl
+//               << "------------------------------------------------------------"
+//                  "--------------"
+//               << std::endl;
+//     return;
+//   } else if (check == 0) {
+//     std::cout << "강의를 추가하지 않았습니다." << std::endl
+//               << "------------------------------------------------------------"
+//                  "--------------"
+//               << std::endl;
+//     return;
+//   }
+// }
 
 
 void modify_lecture(const Lecture& new_lecture) {
